6b3a98517df5812bdfb380779f7b947f3875e19a
Alin Voinea <alin@eaudeweb.ro>
2014-04-11 16:26:45 +0300

    Refs #18962 - Re-organize pdfdiff files

--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
 """ Install
 """
+import os
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "pdfdiff"
-PACKAGE_VERSION = "1.0"
 SUMMARY = (
     "Compare two PDF files and generate diff images "
     "and mark differences with red"
 )
 DESCRIPTION = (
     open("README.rst", 'r').read() + '\n\n' +
-    open('HISTORY.rst', 'r').read()
+    open(os.path.join("docs", 'HISTORY.rst')).read()
 )
+
+VERSION = open('version.txt').read().strip()
 
 setup(
     name=PACKAGE_NAME,
-    version=PACKAGE_VERSION,
+    version=VERSION,
     description=SUMMARY,
     long_description=DESCRIPTION,
     author='Ovidiu Miron',
     author_email='ovidiu.miron@eaudeweb.com',
     url='http://github.com/eea/pdfdiff',
     license='MPL',
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'':'src'},
     entry_points = {
       'console_scripts': [
-          'pdfdiff = pdfdiff.pdfdiff:main'
+          'pdfdiff = pdfdiff:main'
       ]},
     classifiers=[
       'Environment :: Console',
